a21c19ce8a1264676816538ccdd710ea9c6ca9c6
ivayloyordanoff <112658912+ivayloyordanoff@users.noreply.github.com>
2023-11-22 22:58:26 +0200

    Exercises

---
+++ dom-introduction-lab/collect-list-items/collectListItems.js
@@ -0,0 +1,8 @@
+function extractText() {
+  let listItems = document.getElementsByTagName("li");
+  let textArea = document.getElementById("result");
+
+  for (const item of listItems) {
+    textArea.value += item.textContent + "\n";
+  }
+}
---
+++ dom-introduction-lab/extract-parenthesis/extractParenthesis.js
@@ -0,0 +1,14 @@
+function extract(id) {
+  const text = document.getElementById(id).textContent;
+  let pattern = /\(([^)]+)\)/g;
+  let result = [];
+
+  let match = pattern.exec(text);
+
+  while (match) {
+    result.push(match[1]);
+    match = pattern.exec(text);
+  }
+
+  return result.join("; ");
+}
